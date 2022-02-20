f27d9d40319c015749d775f48625cb04ebc836d4
zzzzzzzzzzz <zzzzzzzzzzzgg@gmail.com>
2022-02-20 19:54:33 +0800

    加

--- for11/src/main.rs
+++ for11/src/main.rs
@@ -1,6 +1,7 @@
 use zhscript2_no_std::world_;
 
 fn main() {
+	world_::hello__("显示循环【11】则.了换行");
 	world_::hello__("显示循环【11】【次】则“‘次’ ”了换行");
 	world_::hello__("显示循环【10】【11】【次】则“‘次’ ”了换行");
 	world_::hello__("显示循环【-100】【11】【次】则“‘次’ ”了换行");
